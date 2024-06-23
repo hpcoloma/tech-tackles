12501c139e7b48bcfa020f5886dedb748a12a581
Holly Ann Coloma <holly.life.15@gmail.com>
2024-06-23 12:22:57 +0000

    Create the admin profiles

--- tickets/models.py
+++ tickets/models.py
@@ -2,7 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Ticket(models.Model):
-    STATUS_CHOICES = [
+class Profile(models.Model):
+    ROLES = [
+        ('Admin', 'Admin'),
+        ('User', 'User'),
+        ('IT Support', 'IT Support'),
         
     ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLES, default='User')
+
+
+    def __str__(self):
+        retur self.user.username
